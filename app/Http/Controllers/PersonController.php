cacd8941f3516c010b7cbf5c32ee64e141812739
Nadya610 <247822762+Nadya610@users.noreply.github.com>
2026-02-23 15:59:18 +0100

    Création des controllers

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Inertia\Inertia;
+
+class PersonController extends Controller {
+    
+}
---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use Inertia\Inertia;
+
+class ReservationController extends Controller {
+    
+}
