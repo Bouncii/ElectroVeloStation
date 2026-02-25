9012174bd9119a911043adb68149329dda124ab3
bounci <lulubouncer@gmail.com>
2026-02-25 19:28:10 +0100

    Personcontroller

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,63 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request){
+        $validated = $request->validate([
+            'first_name'         => 'required|string|max:255',
+            'last_name'          => 'required|string|max:255',
+            'age'                => 'required|integer|min:0',
+            'required_bike_size' => 'required|string',
+            'user_id'            => 'required|exists:users,id',
+        ]);
+
+        Person::create($validated);
+        return redirect()->back()->with('success', 'Nouvelle personne ajoutée !');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    
+    public function update(Request $request, Person $person){
+        $validated = $request->validate([
+            'first_name'         => 'required|string|max:255',
+            'last_name'          => 'required|string|max:255',
+            'age'                => 'required|integer|min:0',
+            'required_bike_size' => 'required|string',
+        ]);
+        $person->update($validated);
+        return redirect()->back()->with('success', 'Modifications enregistrées.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
+    public function destroy(Person $person){
+        $person->delete();
+        return redirect()->back()->with('success', 'Personne supprimée de la liste.');
     }
 }
