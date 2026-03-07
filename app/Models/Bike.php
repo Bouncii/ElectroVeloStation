3f3c30b3604dbce5327be578bc652f791e16226e
bounci <lulubouncer@gmail.com>
2026-03-07 18:16:57 +0100

    scope (return available bikes) on bike model

--- app/Models/Bike.php
+++ app/Models/Bike.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Bike extends Model
 {
@@ -12,5 +13,42 @@ class Bike extends Model
 
     public function station() {
         return $this->belongsTo(Station::class);
+    }
+
+    // renvoi les vélos dispos pendant une période et à une station donnée.
+    public function scopeAvailableAtStationOn(Builder $query, $stationId, $startDate, $endDate){
+        return $query
+            ->whereDoesntHave('attributions.reservation', function ($query) use ($startDate, $endDate) {
+                $query->where('start_date', '<', $endDate)
+                  ->where('end_date', '>', $startDate)
+                  ->where('status', '!=', 'cancelled');
+            })
+            
+            ->where(function ($query) use ($stationId, $startDate) {
+                
+                $query->where(function ($q) use ($stationId, $startDate) {
+                    
+                    $q->whereDoesntHave('attributions.reservation', function ($res) use ($startDate) {
+                        $res->where('end_date', '<=', $startDate)
+                            ->where('status', '!=', 'cancelled');
+                    })->where('station_id', $stationId);
+                    
+                })
+                
+                ->orWhereHas('attributions.reservation', function ($query) use ($stationId, $startDate) {
+                    $query->where('end_date', '<=', $startDate)
+                      ->where('return_station_id', $stationId)
+                      ->where('status', '!=', 'cancelled')
+                      
+                      ->whereRaw('reservations.end_date = (
+                          SELECT MAX(r2.end_date) 
+                          FROM reservations r2
+                          JOIN attributions a2 ON r2.id = a2.reservation_id
+                          WHERE a2.bike_id = bikes.id 
+                          AND r2.end_date <= ?
+                          AND r2.status != "cancelled"
+                      )', [$startDate]);
+                });
+            });
     }
 }
