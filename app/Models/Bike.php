2f4c0269519fdfa479163079cf81daea8bfeb420
bounci <lulubouncer@gmail.com>
2026-03-07 20:11:00 +0100

    modified scope

--- app/Models/Bike.php
+++ app/Models/Bike.php
@@ -16,38 +16,52 @@ class Bike extends Model
     }
 
     // renvoi les vélos dispos pendant une période et à une station donnée.
-    public function scopeAvailableAtStationOn(Builder $query, $stationId, $startDate, $endDate){
+    public function scopeAvailableAtStationOn(Builder $query, $stationId, $startDate, $endDate, $ignoreId = null)
+    {
         return $query
-            ->whereDoesntHave('attributions.reservation', function ($query) use ($startDate, $endDate) {
-                $query->where('start_date', '<', $endDate)
-                  ->where('end_date', '>', $startDate)
-                  ->where('status', '!=', 'cancelled');
+            ->whereDoesntHave('attributions.reservation', function ($q) use ($startDate, $endDate, $ignoreId) {
+                $q->where('start_date', '<', $endDate)
+                ->where('end_date', '>', $startDate)
+                ->where('status', '!=', 'cancelled');
+
+                if ($ignoreId) {
+                    $q->where('id', '!=', $ignoreId);
+                }
             })
-            
-            ->where(function ($query) use ($stationId, $startDate) {
+            ->where(function ($query) use ($stationId, $startDate, $ignoreId) {
                 
-                $query->where(function ($q) use ($stationId, $startDate) {
-                    
-                    $q->whereDoesntHave('attributions.reservation', function ($res) use ($startDate) {
+                $query->where(function ($q) use ($stationId, $startDate, $ignoreId) {
+                    $q->whereDoesntHave('attributions.reservation', function ($res) use ($startDate, $ignoreId) {
                         $res->where('end_date', '<=', $startDate)
                             ->where('status', '!=', 'cancelled');
+                        
+                        if ($ignoreId) {
+                            $res->where('id', '!=', $ignoreId);
+                        }
                     })->where('station_id', $stationId);
-                    
                 })
                 
-                ->orWhereHas('attributions.reservation', function ($query) use ($stationId, $startDate) {
-                    $query->where('end_date', '<=', $startDate)
-                      ->where('return_station_id', $stationId)
-                      ->where('status', '!=', 'cancelled')
-                      
-                      ->whereRaw('reservations.end_date = (
-                          SELECT MAX(r2.end_date) 
-                          FROM reservations r2
-                          JOIN attributions a2 ON r2.id = a2.reservation_id
-                          WHERE a2.bike_id = bikes.id 
-                          AND r2.end_date <= ?
-                          AND r2.status != "cancelled"
-                      )', [$startDate]);
+                ->orWhereHas('attributions.reservation', function ($q) use ($stationId, $startDate, $ignoreId) {
+                    $q->where('end_date', '<=', $startDate)
+                    ->where('return_station_id', $stationId)
+                    ->where('status', '!=', 'cancelled');
+
+                    if ($ignoreId) {
+                        $q->where('id', '!=', $ignoreId);
+                    }
+                    $sql = "reservations.end_date = (
+                        SELECT MAX(r.end_date) 
+                        FROM reservations r
+                        JOIN attributions a ON r.id = a.reservation_id
+                        WHERE a.bike_id = bikes.id 
+                        AND r.end_date <= '$startDate'
+                        AND r.status != 'cancelled'";
+
+                    if ($ignoreId) {
+                        $sql .= " AND r.id != $ignoreId";
+                    }
+                    $sql .= ")";
+                    $q->whereRaw($sql);
                 });
             });
     }
