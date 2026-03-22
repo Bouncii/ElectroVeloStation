2d519188e6d741dc9be8894799d04d9f48bc4393
bounci <lulubouncer@gmail.com>
2026-03-22 19:52:08 +0100

    update modele and migration station

--- database/migrations/2026_01_30_122338_create_stations_table.php
+++ database/migrations/2026_01_30_122338_create_stations_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->decimal('latitude', 10, 8)->nullable(); 
+            $table->decimal('longitude', 11, 8)->nullable();
             $table->timestamps();
         });
     }
