8983e088ec0e212ddc55b46d9871727f3458be4a
Bouncii <lulubouncer@gmail.com>
2026-05-11 12:46:41 +0200

    fix mail resa

--- resources/views/emails/reservation.blade.php
+++ resources/views/emails/reservation.blade.php
@@ -6,7 +6,7 @@
     <body>
         <p>Bonjour,</p>
         <p>Votre réservation du <strong>{{ \Carbon\Carbon::parse($reservation->start_date)->format('d/m/Y à H:i') }}</strong> au <strong>{{ \Carbon\Carbon::parse($reservation->end_date)->format('d/m/Y à H:i') }}</strong> a bien été enregistrée.</p>
-        <a href="/profile/" style="display:inline-block; padding:10px 20px; background-color:#28a745; color:white; text-decoration:none; border-radius:5px;">
+        <a href="{{ route('profile.index') }}" style="display:inline-block; padding:10px 20px; background-color:#28a745; color:white; text-decoration:none; border-radius:5px;">
             Voir ma réservation
         </a>
         <p>L'équipe d'Electro Vélo Station</p>
